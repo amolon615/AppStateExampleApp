c0b836eb8a4d25c7ba8e9309ce3334e6f2c93967
Artem <114776102+amolon615@users.noreply.github.com>
2023-12-27 15:05:15 +0100

    Initial Commit

---
+++ AppStateExampleApp/AppStateExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStateExampleAppApp.swift
+//  AppStateExampleApp
+//
+//  Created by amolonus on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppStateExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
